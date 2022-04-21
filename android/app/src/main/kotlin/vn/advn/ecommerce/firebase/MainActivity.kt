dc527eea36925210ad5584a699893a5cbfefb47e
fluster <tuananhpham0706@gmail.com>
2022-04-21 09:19:09 +0700

    update

---
+++ android/app/src/main/kotlin/vn/advn/ecommerce/firebase/MainActivity.kt
@@ -0,0 +1,16 @@
+package vn.advn.ecommerce.firebase
+
+//import androidx.annotation.NonNull;
+
+import io.flutter.embedding.android.FlutterActivity
+
+//import io.flutter.embedding.engine.FlutterEngine;
+//import io.flutter.plugins.GeneratedPluginRegistrant;
+
+//import io.flutter.embedding.engine.FlutterEngine;
+//import io.flutter.plugins.GeneratedPluginRegistrant;
+class MainActivity : FlutterActivity() { //  @Override
+    //  public void configureFlutterEngine(@NonNull FlutterEngine flutterEngine) {
+    //    GeneratedPluginRegistrant.registerWith(flutterEngine);
+    //  }
+}
